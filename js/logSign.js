aef79994c55dd6dbb3865e10cc56594ebaa5f7d8
rahulrk2303 <30636208+rahulrk2303@users.noreply.github.com>
2019-02-10 06:19:17 +0000

    Add files via upload

--- js/logSign.js
+++ js/logSign.js
@@ -12,3 +12,37 @@ function viewsignup() {
   y.style.display = "block";
   x.style.display = "none";
 }
+
+function riderviewbook() {
+	var a = document.getElementById("bookride");
+	var b = document.getElementById("riderprofile");
+	var c = document.getElementById("ridertrips");
+	// var d = document.getElementById("logout");
+	a.style.display = "block";
+	b.style.display = "none";
+	c.style.display = "none";
+	// d.style.display = "none";
+}
+
+function riderviewprofile() {
+	var a = document.getElementById("bookride");
+	var b = document.getElementById("riderprofile");
+	var c = document.getElementById("ridertrips");
+	// var d = document.getElementById("logout");
+	a.style.display = "none";
+	b.style.display = "block";
+	c.style.display = "none";
+	// d.style.display = "none";
+}
+
+function riderviewtrips() {
+	var a = document.getElementById("bookride");
+	var b = document.getElementById("riderprofile");
+	var c = document.getElementById("ridertrips");
+	// var d = document.getElementById("logout");
+	a.style.display = "none";
+	b.style.display = "none";
+	c.style.display = "block";
+	// d.style.display = "none";
+}
+
