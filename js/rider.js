59c5035cc997d5569a7d2634c15674d9abf5f1c3
rahulrk2303 <30636208+rahulrk2303@users.noreply.github.com>
2019-02-13 17:20:39 +0000

    Add files via upload

---
+++ js/rider.js
@@ -0,0 +1,32 @@
+function riderviewbook() {
+	var a = document.getElementById("bookride");
+	var b = document.getElementById("riderprofile");
+	var c = document.getElementById("ridertrips");
+	// var d = document.getElementById("logout");
+	a.style.display = "block";
+	b.style.display = "none";
+	c.style.display = "none";
+	// d.style.display = "none";
+}
+
+function riderviewprofile() {
+	var a = document.getElementById("bookride");
+	var b = document.getElementById("riderprofile");
+	var c = document.getElementById("ridertrips");
+	// var d = document.getElementById("logout");
+	a.style.display = "none";
+	b.style.display = "block";
+	c.style.display = "none";
+	// d.style.display = "none";
+}
+
+function riderviewtrips() {
+	var a = document.getElementById("bookride");
+	var b = document.getElementById("riderprofile");
+	var c = document.getElementById("ridertrips");
+	// var d = document.getElementById("logout");
+	a.style.display = "none";
+	b.style.display = "none";
+	c.style.display = "block";
+	// d.style.display = "none";
+}
