8a4396fe8f65a4626f45c9f17bbe9054223fa7d8
rahulrk2303 <30636208+rahulrk2303@users.noreply.github.com>
2019-02-14 17:04:03 +0000

    Add files via upload

--- js/driver.js
+++ js/driver.js
@@ -41,3 +41,50 @@ function drivercompleted() {
 	//c.style.display = "none";
 	d.style.display = "block";
 }
+
+// function all () {
+// 	var a = document.getElementById("upcoming");
+// 	var b = document.getElementById("completed");
+// 	var c = document.getElementById("cancelled");
+// 	a.style.display = "block";
+// 	b.style.display = "block";
+// 	c.style.display = "block";
+// 	var d = document.getElementById("all");
+// 	d.style.display = "block";
+// }
+
+function upcoming () {
+	var a = document.getElementById("upcoming");
+	var b = document.getElementById("completed");
+	var c = document.getElementById("cancelled");
+	a.style.display = "block";
+	b.style.display = "none";
+	c.style.display = "none";
+}
+
+function completed () {
+	var a = document.getElementById("upcoming");
+	var b = document.getElementById("completed");
+	var c = document.getElementById("cancelled");
+	a.style.display = "none";
+	b.style.display = "block";
+	c.style.display = "none";
+}
+
+function cancelled () {
+	var a = document.getElementById("upcoming");
+	var b = document.getElementById("completed");
+	var c = document.getElementById("cancelled");
+	a.style.display = "none";
+	b.style.display = "none";
+	c.style.display = "block";
+}
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
--- js/rider.js
+++ js/rider.js
@@ -30,3 +30,40 @@ function riderviewtrips() {
 	c.style.display = "block";
 	// d.style.display = "none";
 }
+
+
+function upcoming () {
+	var a = document.getElementById("upcoming");
+	var b = document.getElementById("completed");
+	var c = document.getElementById("cancelled");
+	a.style.display = "block";
+	b.style.display = "none";
+	c.style.display = "none";
+}
+
+function completed () {
+	var a = document.getElementById("upcoming");
+	var b = document.getElementById("completed");
+	var c = document.getElementById("cancelled");
+	a.style.display = "none";
+	b.style.display = "block";
+	c.style.display = "none";
+}
+
+function cancelled () {
+	var a = document.getElementById("upcoming");
+	var b = document.getElementById("completed");
+	var c = document.getElementById("cancelled");
+	a.style.display = "none";
+	b.style.display = "none";
+	c.style.display = "block";
+}
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
