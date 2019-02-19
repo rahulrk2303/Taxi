e3af52fdeb91dd6e7d27de5a414a79f4ff29b402
rahulrk2303 <30636208+rahulrk2303@users.noreply.github.com>
2019-02-20 00:09:55 +0530

    Add files via upload

--- js/rider.js
+++ js/rider.js
@@ -59,11 +59,3 @@ function cancelled () {
 	c.style.display = "block";
 }
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-} 
